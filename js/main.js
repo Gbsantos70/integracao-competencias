76878d70757a451cdffbb4973bb64d2a9f3db9e1
Gbsantos70 <gabriel.s.souza8@aluno.senai.br>
2023-12-26 13:29:40 -0300

    borderRadius

--- js/main.js
+++ js/main.js
@@ -8,13 +8,12 @@ const people = () => {
   const dataContainer = document.getElementById("data-container");
 
   getAllPeople().then((resp) => {
-    // Filtra as pessoas desejadas (Luke, Darth Vader, Obi-Wan)
+   
     const filteredPeople = resp.results.filter((person) =>
       ["Luke Skywalker", "Darth Vader", "Obi-Wan Kenobi"].includes(person.name)
     );
 
-    // Mapeia as pessoas filtradas para criar os cards
-    filteredPeople.forEach((person) => {
+      filteredPeople.forEach((person) => {
       const cardElement = createCardElement(person);
       dataContainer.appendChild(cardElement);
     });
@@ -23,9 +22,6 @@ const people = () => {
 
 const createCardElement = (person) => {
   const card = document.createElement("div");
-
-  
-  // Determina o tipo de card com base no nome da pessoa
   let cardType;
   if (person.name.toLowerCase().includes("luke")) {
     cardType = "card";
@@ -39,8 +35,8 @@ const createCardElement = (person) => {
 
   const imgElement = document.createElement("div");
   imgElement.className = "img";
- imgElement.innerHTML=`<img src='img/${person.name.toLowerCase().replace(/\s/g, '-')}LUKE.jpg' alt='${person.name}'>`;
-  imgElement.innerHTML = `<span>${person.name}</span>`;
+  // imgElement.innerHTML = `<img src='img/${person.name.toLowerCase().replace(/\s/g, '-')}LUKE2.jpg' alt='${person.name}'>`;
+  // imgElement.innerHTML += `<span>${person.name}</span>`;
 
   const contentElement = document.createElement("div");
   contentElement.className = "content";
@@ -48,8 +44,27 @@ const createCardElement = (person) => {
     <span class="title">${person.name}</span>
     <p class="desc">Gender: ${person.gender}</p>
     <p class="desc">Birth Year: ${person.birth_year}</p>
-    <p class="desc">Eye Color: ${person.eye_color}</p>
+    <p class="desc">Eye color: ${person.eye_color}</p>
+    <p class="desc">Hair color: ${person.hair_color}</p>
+    <p class="desc">: ${person.height}</p>
   `;
+
+  const ratingElement = document.createElement("div");
+  ratingElement.className = "rating";
+  const stars = Array.from({ length: 5 }, (_, index) => index + 1);
+
+  stars.forEach((starIndex) => {
+    const starIcon = document.createElement("span");
+    starIcon.innerHTML = "&#9733;"; 
+    starIcon.classList.add("star");
+
+  
+    starIcon.addEventListener("click", () => {
+      setRating(card, starIndex);
+    });
+
+    ratingElement.appendChild(starIcon);
+  });
 
   const arrowElement = document.createElement("div");
   arrowElement.className = "arrow";
@@ -57,15 +72,19 @@ const createCardElement = (person) => {
 
   card.appendChild(imgElement);
   card.appendChild(contentElement);
+  card.appendChild(ratingElement);
   card.appendChild(arrowElement);
+ 
 
-  // Adiciona eventos de hover para cada card
+
+
   card.addEventListener("mouseover", () => {
     imgElement.style.opacity = "1";
     imgElement.style.height = "200px";
     contentElement.style.padding = "20px";
     contentElement.style.height = "260px";
     contentElement.style.transform = "translateY(0px)";
+   
   });
 
   card.addEventListener("mouseout", () => {
@@ -74,12 +93,25 @@ const createCardElement = (person) => {
     contentElement.style.padding = "0 20px";
     contentElement.style.height = "0";
     contentElement.style.transform = "translateY(30px)";
+   
   });
 
   return card;
-
-
 };
+
+
+const setRating = (card, rating) => {
+  const stars = card.querySelectorAll(".star");
+
+  stars.forEach((star, index) => {
+    if (index < rating) {
+      star.classList.add("selected");
+    } else {
+      star.classList.remove("selected");
+    }
+  });
+};
+
 
 
 
